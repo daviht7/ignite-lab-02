40ab34679cda56f016180d8035bef6c24489d6c4
Davi Holanda <daviht7@gmail.com>
2022-06-25 21:42:17 -0300

    finalizando subscribe

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,6 +2,9 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oqific0u9r01xkdlxwc3e4/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization' : `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
